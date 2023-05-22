fff6226369b0aedd5d2cf070785057f76c5c8b76
Julie Souvandy <julie.souvandy.pro@gmail.com>
2023-05-22 23:28:11 +0200

    ajout des boutons décroissants et avec description

--- pieces.js
+++ pieces.js
@@ -59,10 +59,42 @@ for (let i = 0; i < pieces.length; i++) {
 }
 
 const boutonTrier = document.querySelector(".btn-trier");
-boutonTrier.addEventListener("click", function() {
+
+boutonTrier.addEventListener("click", function () {
     const piecesOrdonnees = Array.from(pieces);
     piecesOrdonnees.sort(function(a,b){
         return a.prix - b.prix;
     });
-    console.log(pieces);
+    console.log(piecesOrdonnees);
+});
+
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function(){
+    const piecesDecroissant = Array.from(pieces);
+    piecesDecroissant.sort(function(a,b){
+        return b.prix - a.prix;
+    });
+    console.log(piecesDecroissant);
+});
+
+
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function ()  {
+    const piecesFiltrees = pieces.filter( function (piece) {
+    return piece.prix <=35 ;
+    });
+    console.log(piecesFiltrees);
+});
+
+const boutonDescription = document.querySelector(".btn-description");
+
+boutonDescription.addEventListener("click", function () {
+    const piecesAvecDescription = pieces.filter(function (piece){
+    return piece.description;
+    // je ne sais pas comment dire : s'il y a une description
+    });
+    console.log(piecesAvecDescription);
 });
